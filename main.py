85168f70a0de74c7b7147bee42bf48dc938c8513
favourosa-tech <favourosaatsenokhai@gmail.com>
2025-09-07 18:26:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create Window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title('favour Window')
+window.geometry('600x700')
+window.mainloop()
